1fdc93a97c63b4ea91128ce6fe18746ab56d7339
ykoitzsch <y.koitzsch@gmx.at>
2019-02-06 20:29:07 +0100

    remove messaging code which does not belong here

--- ratings/src/main/java/com/jhipster/bachelor/ratings/service/RatingService.java
+++ ratings/src/main/java/com/jhipster/bachelor/ratings/service/RatingService.java
@@ -1,16 +1,15 @@
 package com.jhipster.bachelor.ratings.service;
 
-import com.jhipster.bachelor.ratings.domain.Rating;
-import com.jhipster.bachelor.ratings.repository.RatingRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
+import com.jhipster.bachelor.ratings.domain.Rating;
+import com.jhipster.bachelor.ratings.repository.RatingRepository;
 
 /**
  * Service Implementation for managing Rating.
@@ -19,68 +18,59 @@ import java.util.Optional;
 @Transactional
 public class RatingService {
 
-    private final Logger log = LoggerFactory.getLogger(RatingService.class);
+  private final Logger log = LoggerFactory.getLogger(RatingService.class);
 
-    private RatingRepository ratingRepository;
+  private RatingRepository ratingRepository;
 
-    public RatingService(RatingRepository ratingRepository) {
-        this.ratingRepository = ratingRepository;
-    }
+  public RatingService(RatingRepository ratingRepository) {
+    this.ratingRepository = ratingRepository;
+  }
 
-    
-    @StreamListener(Processor.INPUT)
-    public void registerRating(Rating rating) {
-    	log.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-    	log.debug(rating.toString());
-    }
-    
-    
-    /**
-     * Save a rating.
-     *
-     * @param rating the entity to save
-     * @return the persisted entity
-     */
-    public Rating save(Rating rating) {
-        log.debug("Request to save Rating : {}", rating);
-        return ratingRepository.save(rating);
-    }
+  /**
+   * Save a rating.
+   *
+   * @param rating the entity to save
+   * @return the persisted entity
+   */
+  public Rating save(Rating rating) {
+    log.debug("Request to save Rating : {}", rating);
+    return ratingRepository.save(rating);
+  }
 
-    /**
-     * Get all the ratings.
-     *
-     * @return the list of entities
-     */
-    @Transactional(readOnly = true)
-    public List<Rating> findAll() {
-        log.debug("Request to get all Ratings");
-        return ratingRepository.findAll();
-    }
+  /**
+   * Get all the ratings.
+   *
+   * @return the list of entities
+   */
+  @Transactional(readOnly = true)
+  public List<Rating> findAll() {
+    log.debug("Request to get all Ratings");
+    return ratingRepository.findAll();
+  }
 
+  /**
+   * Get one rating by id.
+   *
+   * @param id the id of the entity
+   * @return the entity
+   */
+  @Transactional(readOnly = true)
+  public Optional<Rating> findOne(Long id) {
+    log.debug("Request to get Rating : {}", id);
+    return ratingRepository.findById(id);
+  }
 
-    /**
-     * Get one rating by id.
-     *
-     * @param id the id of the entity
-     * @return the entity
-     */
-    @Transactional(readOnly = true)
-    public Optional<Rating> findOne(Long id) {
-        log.debug("Request to get Rating : {}", id);
-        return ratingRepository.findById(id);
-    }
+  /**
+   * Delete the rating by id.
+   *
+   * @param id the id of the entity
+   */
+  public void delete(Long id) {
+    log.debug("Request to delete Rating : {}", id);
+    ratingRepository.deleteById(id);
+  }
 
-    /**
-     * Delete the rating by id.
-     *
-     * @param id the id of the entity
-     */
-    public void delete(Long id) {
-        log.debug("Request to delete Rating : {}", id);
-        ratingRepository.deleteById(id);
-    }
-
-	public List<Rating> findByProductId(String productId) {
-		return ratingRepository.findByProductId(Long.valueOf(productId));
-	}
+  public List<Rating> findByProductId(String productId) {
+    return ratingRepository.findByProductId(Long.valueOf(productId));
+  }
 }
